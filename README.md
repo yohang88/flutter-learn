f2b38b06b7a02e94e004865e844c4e8ab90d3cfd
Yoga Hanggara <yohang88@gmail.com>
2019-04-13 16:59:31 +0700

    google maps, modify Readme
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# flutter_app
+# Learn Flutter
 
-A new Flutter application.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## To Do
+- [x] Scaffolding basic screens
+- [x] Bottom Navigation Bar
+- [x] Floating Action Button
+- [x] Image
+- [x] Some layout enhancements
+- [x] Multiple Page/Widget
+- [x] Basic State Management
+- [x] Google Maps
+- [ ] Consume REST API, Repository, Offline
+- [ ] Enhance Global State Management
--- lib/screens/main/messages.dart
+++ lib/screens/main/messages.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MessagesWidget extends StatefulWidget {
   MessagesWidget({Key key, this.counter}) : super(key: key);
@@ -10,6 +13,14 @@ class MessagesWidget extends StatefulWidget {
 }
 
 class _MessageState extends State<MessagesWidget> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -99,7 +110,16 @@ class _MessageState extends State<MessagesWidget> {
 
     return ListView(
       children: [
-        Image.asset('images/lake2.jpg'),
+        Container(
+          height: 200,
+          child: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+          ),
+        ),
         titleSection,
         card
       ]
